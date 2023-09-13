b1765fae369c0f6061c183fb16db1d4d457a08d9
pirotyyy <hiroki1423@docomo.ne.jp>
2023-09-13 16:28:21 +0900

    feat: DB初期化時に値を入れる

--- .build/db/initdb.d/01_init.sql
+++ .build/db/initdb.d/01_init.sql
@@ -6,3 +6,24 @@ CREATE TABLE IF NOT EXISTS `user` (
 `name` VARCHAR(8) NOT NULL,
 `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (`user_id`));
+
+CREATE TABLE IF NOT EXISTS `charactor` (
+`charactor_id` INT AUTO_INCREMENT NOT NULL,
+`name` VARCHAR(255) NOT NULL, PRIMARY KEY (`charactor_id`));
+
+
+INSERT INTO `user` (`name`, `token`)
+VALUES
+  ('user1', 'aaaaa'),
+  ('user2', 'bbbbb'),
+  ('user3', 'ccccc'),
+  ('user4', 'ddddd'),
+  ('user5', 'eeeee');
+
+INSERT INTO `charactor` (`name`)
+VALUES
+  ('Charactor A'),
+  ('Charactor B'),
+  ('Charactor C'),
+  ('Charactor D'),
+  ('Charactor E');

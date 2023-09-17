4cf45ce4d8655346e1b58c2d6516991e29a73be1
pirotyyy <hiroki1423@docomo.ne.jp>
2023-09-17 17:20:58 +0900

    fix: idをauto_incrementする

--- .build/db/initdb.d/01_init.sql
+++ .build/db/initdb.d/01_init.sql
@@ -1,54 +1,23 @@
 USE ca_db;
 
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` VARCHAR(64) NOT NULL,
+`id` INT NOT NULL AUTO_INCREMENT,
 `token` VARCHAR(255) NOT NULL,
 `name` VARCHAR(32) NOT NULL,
 `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (`user_id`));
-
-DELIMITER //
-CREATE TRIGGER `user_before_insert` BEFORE INSERT ON `user`
-FOR EACH ROW
-BEGIN
-  SET NEW.`user_id` = CONCAT('user-', UUID());
-END;
-//
-DELIMITER ;
+PRIMARY KEY (`id`));
 
 CREATE TABLE IF NOT EXISTS `game_character` (
-`character_id` VARCHAR(64) NOT NULL,
+`id` INT NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(255) NOT NULL,
-`rarity` VARCHAR(8) NOT NULL, PRIMARY KEY (`character_id`));
-
-DELIMITER //
-CREATE TRIGGER `game_character_before_insert` BEFORE INSERT ON `game_character`
-FOR EACH ROW
-BEGIN
-  SET NEW.`character_id` = CONCAT('character-', UUID());
-END;
-//
-DELIMITER ;
-
+`rarity` VARCHAR(8) NOT NULL, PRIMARY KEY (`id`));
 
 CREATE TABLE IF NOT EXISTS `user_character` (
-  `user_character_id` VARCHAR(64) PRIMARY KEY NOT NULL,
-  `user_id` VARCHAR(64) NOT NULL,
-  `character_id` VARCHAR(64) NOT NULL,
-  `acquired_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`character_id`) REFERENCES `game_character` (`character_id`)
-);
-
-DELIMITER //
-CREATE TRIGGER `user_character_before_insert` BEFORE INSERT ON `user_character`
-FOR EACH ROW
-BEGIN
-  SET NEW.`user_character_id` = CONCAT('user-character-', UUID());
-END;
-//
-DELIMITER ;
-
-
+`id` INT NOT NULL AUTO_INCREMENT,
+`user_id` INT NOT NULL,
+`character_id` INT NOT NULL,
+`acquired_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`),
+FOREIGN KEY (`character_id`) REFERENCES `game_character` (`id`));
 
 INSERT INTO `user` (`name`, `token`)
 VALUES

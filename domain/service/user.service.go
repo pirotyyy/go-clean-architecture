e6b23b16f24a51c0f71527cd935e3237c39d8fdd
pirotyyy <hiroki1423@docomo.ne.jp>
2023-10-04 00:06:12 +0900

    feat: service内で検証を行う

--- domain/service/user.service.go
+++ domain/service/user.service.go
@@ -4,6 +4,7 @@ import (
 	"ca-tech/domain/model"
 	"ca-tech/domain/repository"
 	"context"
+	"errors"
 )
 
 type UserService interface {
@@ -23,13 +24,39 @@ func NewUserService(ur repository.UserRepository) UserService {
 }
 
 func (us *userService) CreateUser(ctx context.Context, name string) (*model.User, error) {
+	if !isNameValid(name) {
+		return nil, errors.New("name format is invalid")
+	}
 	return us.userRepo.CreateUser(ctx, name)
 }
 
 func (us *userService) GetUser(ctx context.Context, token string) (*model.User, error) {
+	if !isTokenValid(token) {
+		return nil, errors.New("token format is invalid")
+	}
 	return us.userRepo.GetUserByToken(ctx, token)
 }
 
 func (us *userService) UpdateUser(ctx context.Context, name string, token string) (*model.User, error) {
+	if !isNameValid(name) {
+		return nil, errors.New("name format is invalid")
+	}
+	if !isTokenValid(token) {
+		return nil, errors.New("token format is invalid")
+	}
 	return us.userRepo.UpdateUser(ctx, name, token)
+}
+
+func isNameValid(name string) bool {
+	if name == "" || (len(name) > 20) {
+		return false
+	}
+	return true
+}
+
+func isTokenValid(token string) bool {
+	if token == "" {
+		return false
+	}
+	return true
 }

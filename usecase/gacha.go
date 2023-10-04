e0a31aaae6bc92a54adc7727030802293210eb97
pirotyyy <hiroki1423@docomo.ne.jp>
2023-10-04 18:22:02 +0900

    feat: serviceを組み合わせてgachaのusecaseを実装

--- usecase/gacha.go
+++ usecase/gacha.go
@@ -11,21 +11,80 @@ type GachaUsecase interface {
 }
 
 type gachaUsecase struct {
-	svc service.GachaService
+	userService      service.UserService
+	charaSerivce     service.CharacterService
+	gachaService     service.GachaService
+	userCharaService service.UserCharacterService
 }
 
-func NewGachaUsecase(gs service.GachaService) GachaUsecase {
+func NewGachaUsecase(us service.UserService, cs service.CharacterService, gs service.GachaService, ucs service.UserCharacterService) GachaUsecase {
 	return &gachaUsecase{
-		svc: gs,
+		userService:      us,
+		charaSerivce:     cs,
+		gachaService:     gs,
+		userCharaService: ucs,
 	}
 }
 
 func (gu *gachaUsecase) Draw(ctx context.Context, times int64, token string) ([]*model.Character, error) {
+	var resultCharacters []*model.Character
+
 	// user
-	// gacha service
-	err := gu.svc.InitCharacterList(ctx)
+	user, err := gu.userService.GetUser(ctx, token)
 	if err != nil {
 		return nil, err
 	}
-	return gu.svc.Draw(ctx, times, token)
+	// draw gacha
+	gachaResults, err := gu.gachaService.Draw(ctx, times)
+	if err != nil {
+		return nil, err
+	}
+
+	characters, err := gu.charaSerivce.GetCharacters(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, gachaResult := range gachaResults {
+		rarity := getRarity(gachaResult.Rarity)
+		targetCharacters := getTargetCharacters(characters, rarity)
+		targetCharacter := targetCharacters[gachaResult.Index%int64(len(targetCharacters))]
+		resultCharacters = append(resultCharacters, targetCharacter)
+	}
+
+	// save user_character
+	for _, resultCharacter := range resultCharacters {
+		err := gu.userCharaService.Insert(ctx, user.UserId, resultCharacter.CharacterID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return resultCharacters, nil
+}
+
+func getTargetCharacters(characters []*model.Character, rarity model.Rarity) []*model.Character {
+	var targetCharacters []*model.Character
+
+	for _, char := range characters {
+		if char.Rarity == rarity {
+			targetCharacters = append(targetCharacters, char)
+		}
+	}
+
+	return targetCharacters
+}
+
+func getRarity(randNum int64) model.Rarity {
+
+	switch {
+	case randNum < 50:
+		return model.N
+	case randNum < 70:
+		return model.R
+	case randNum < 90:
+		return model.SR
+	default:
+		return model.SSR
+	}
 }
